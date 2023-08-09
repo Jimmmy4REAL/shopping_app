2768d948c1b4b3b1d042d155e0d85fc2cb69f11d
Jimmmy Ye <jimmmy@Jimmys-MacBook-Pro.local>
2023-08-09 22:52:45 +0100

    completed inter-communication between order-service and inventory-service using web-flux, moving to service discovery

--- inventory-service/src/main/java/jimmy/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/jimmy/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package jimmy.inventoryservice.controller;
 
 import jimmy.inventoryservice.service.InventoryService;
+import jimmy.orderservice.dto.InventoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -11,10 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class InventoryController {
 
     private final InventoryService inventoryService;
+// http://localhost:8082/api/inventory/iphone-13,iphone13-red
 
-    @GetMapping("/{sku-code}")
+    // current address format > http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- order-service/src/main/java/jimmy/orderservice/service/OrderService.java
+++ order-service/src/main/java/jimmy/orderservice/service/OrderService.java
@@ -1,6 +1,7 @@
 package jimmy.orderservice.service;
 
 
+import jimmy.orderservice.dto.InventoryResponse;
 import jimmy.orderservice.dto.OrderLineItemsDto;
 import jimmy.orderservice.dto.OrderRequest;
 import jimmy.orderservice.model.Order;
@@ -9,16 +10,18 @@ import jimmy.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -31,7 +34,28 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call Inventory Service, and place order if product is in
+        // stock
+        InventoryResponse[] inventoryResponsArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock){
+            System.out.println("Jimmy confirm successful in stock");
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
